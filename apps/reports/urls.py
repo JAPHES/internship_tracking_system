f8c95fdb09cd97222575b37fe6acf6275c898a67
JAPHES <japhesmurithi@gmail.com>
2026-09-15 22:14:38 +0300

    feat: add apps/reports/urls.py

---
+++ apps/reports/urls.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import WeeklyReportViewSet
+
+router = DefaultRouter()
+router.register("", WeeklyReportViewSet, basename="report")
+urlpatterns = router.urls
