04731809237be89994159d50cd59249b6469341e
JAPHES <japhesmurithi@gmail.com>
2026-09-15 22:13:48 +0300

    feat: add apps/accounts/managers.py

---
+++ apps/accounts/managers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email: str, password: str | None = None, **extra_fields):
+        if not email:
+            raise ValueError("An email address is required.")
+        email = self.normalize_email(email).lower()
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.full_clean(exclude={"password"})
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email: str, password: str, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("role", "ADMIN")
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("A superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("A superuser must have is_superuser=True.")
+        if extra_fields.get("role") != "ADMIN":
+            raise ValueError("A superuser must have the ADMIN role.")
+        return self.create_user(email, password, **extra_fields)
