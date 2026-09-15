68b4fe2e31979004b29b9a1cc8d76be80772033e
JAPHES <japhesmurithi@gmail.com>
2026-09-15 22:15:00 +0300

    feat: add apps/tracks/urls.py

---
+++ apps/tracks/urls.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import TrackViewSet
+
+router = DefaultRouter()
+router.register("", TrackViewSet, basename="track")
+urlpatterns = router.urls
