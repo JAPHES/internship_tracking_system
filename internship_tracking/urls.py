e357d0847d80b59f49bdc1a8d410cc680599dcb9
JAPHES <japhesmurithi@gmail.com>
2026-09-16 22:52:53 +0300

    feat: redirect root URL to API documentation

--- internship_tracking/urls.py
+++ internship_tracking/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="swagger-ui", permanent=False), name="home"),
     path("admin/", admin.site.urls),
     path("api/v1/auth/", include("apps.accounts.urls")),
     path("api/v1/students/", include("apps.students.urls")),
