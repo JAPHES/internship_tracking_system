721af57a758e10b7e1f798e09a23c47577cde9ea
JAPHES <japhesmurithi@gmail.com>
2026-09-16 23:24:20 +0300

    test: use non-manifest static storage

--- internship_tracking/settings/test.py
+++ internship_tracking/settings/test.py
@@ -6,6 +6,10 @@ from .base import *  # noqa: E402,F403
 
 DEBUG = False
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+STORAGES = {
+    **STORAGES,  # noqa: F405
+    "staticfiles": {"BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage"},
+}
 MIDDLEWARE = [
     middleware
     for middleware in MIDDLEWARE  # noqa: F405
