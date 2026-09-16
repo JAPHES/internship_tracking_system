e2d3f20776e9f5e77c483693a6affd46901e360f
JAPHES <japhesmurithi@gmail.com>
2026-09-16 21:58:37 +0300

    chore: disable WhiteNoise during development

--- internship_tracking/settings/development.py
+++ internship_tracking/settings/development.py
@@ -2,4 +2,10 @@ from .base import *  # noqa: F403
 
 DEBUG = env.bool("DEBUG", default=True)  # noqa: F405
 
+MIDDLEWARE = [
+    middleware
+    for middleware in MIDDLEWARE  # noqa: F405
+    if middleware != "whitenoise.middleware.WhiteNoiseMiddleware"
+]
+
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
